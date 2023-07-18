995ca6bf46008f36de817285d2a2325aeed77026
walterfrancocordova <walter.franco.-nd@disney.com>
2023-07-18 07:07:14 -0600

    cambios

---
+++ src/main/java/com/example/demo2/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.example.demo2.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+//@Configuration
+//@EnableWebSecurity
+public class SecurityConfig {
+
+    public static final String[] ENDPOINTS_WHITELIST = {
+            "/",
+    };
+
+
+  //  @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.authorizeRequests(request ->
+                        request.antMatchers(ENDPOINTS_WHITELIST).permitAll()
+                                .anyRequest().authenticated())
+                .csrf().disable()
+                .build();
+    }
+    //@Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
