499defd6747465d36c6d76a61d60f80e6abc9d7d
walterfrancocordova <walter.franco.-nd@disney.com>
2023-07-24 02:36:02 -0600

    aaaa

--- src/main/java/com/example/demo2/config/SwaggerConfig.java
+++ src/main/java/com/example/demo2/config/SwaggerConfig.java
@@ -1,6 +1,12 @@
 package com.example.demo2.config;
 
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,19 +16,28 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger.web.*;
+import com.google.common.collect.Sets;
+
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+
+    private static final String AUTHORIZATION_HEADER = "Authorization";
+
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.example.demo2"))
-                .paths(PathSelectors.any())
-                .build()
-                .apiInfo(apiInfo());
+    public Docket apiDocket() {
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo())
+                .securityContexts(List.of(securityContext()))
+                .securitySchemes(List.of(apiKey()))
+                .consumes(Sets.newHashSet(MediaType.APPLICATION_JSON_VALUE))
+                .produces(Sets.newHashSet(MediaType.APPLICATION_JSON_VALUE))
+                .select().apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+                .paths(PathSelectors.any()).build();
     }
+
+
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
@@ -41,4 +56,24 @@ public class SwaggerConfig {
                 .defaultModelExpandDepth(1)
                 .build();
     }
+
+
+
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", AUTHORIZATION_HEADER, "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder().securityReferences(defaultAuth()).build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return List.of(new SecurityReference("JWT", authorizationScopes));
+    }
+
+
+
 }
