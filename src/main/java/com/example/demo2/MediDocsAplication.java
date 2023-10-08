8e562164395c2d8844a628411604967ec46e3a1f
Rextro <223275@ids.upchiapas.edu.mx>
2023-10-08 16:49:19 -0600

    comentario

--- src/main/java/com/example/demo2/MediDocsAplication.java
+++ src/main/java/com/example/demo2/MediDocsAplication.java
@@ -7,7 +7,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @SpringBootApplication
 @EnableTransactionManagement
 public class MediDocsAplication {
-
+	//si
 	public static void main(String[] args) {
 		SpringApplication.run(MediDocsAplication.class, args);
 	}
